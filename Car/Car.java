84c208e7f6f96b479e158b6e9251233c4ad1efed
hkon123 <hkonhd@gmail.com>
2020-11-09 12:59:18 +0100

    Finished Exercise 4

--- Car/Car.java
+++ Car/Car.java
@@ -27,6 +27,25 @@ class Car
     {
         return price;
     }
+    
+    public void setRegistration(String regIn) {
+    	registration = regIn;
+    }
+    
+    public void setMake(String makeIn) {
+    	make = makeIn;
+    }
+    
+    public void setPrice(double priceIn) {
+    	price = priceIn;
+    }
+    
+    public void displayDetails() {
+    	System.out.printf("The car has registration number: %s%n", getRegistration());
+    	System.out.printf("The car is of make: %s%n", getMake());
+    	System.out.printf("The price of the car is: %f%n", getPrice());
+    }
+    
 }
 
 

aa1d32793bd7aac2dd3ea2880fe194fcc6a97afa
hkon123 <hkonhd@gmail.com>
2020-11-09 15:41:32 +0100

    Finished Exercise 1

--- Week2/Lab 2/BrownPaperPackages/WrappedUp.java
+++ Week2/Lab 2/BrownPaperPackages/WrappedUp.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Favourite class to test a string object and it's methods.
  * 
@@ -10,18 +10,25 @@ public class WrappedUp
 	
 	public void guessMyFavouriteThing()
 	{
+		Scanner sc = new Scanner(System.in);
 	   //Create a string object with your favourite animal, car ..whatever
 	   //e.g String favMotorBikeManufacturer = "bmw"
+		String favThing = "Snus";
 	   
 	   /* Now prompt for a guess - using the Scanner class
+	    * 
 	   ** Using String methods, you should give the length 
 	   ** and first letter of your favourite thing in the prompt.
 	   */
-	  
-	  
-	  
+		System.out.printf("Guess my favourite thing! It starts with an %s and has %d characters: ",favThing.charAt(0), favThing.length());
+		String guess = sc.next();
 	  // Now test if the guess was correct - ignore case 
-	 
+		if (favThing.equalsIgnoreCase(guess)) {
+			System.out.printf("Correct! My favourite thing is: %s%n", guess);
+		}
+		else {
+			System.out.println("WRONG!");
+		}
 	
 	
 	
@@ -30,6 +37,7 @@ public class WrappedUp
 
 	public static void main(String args[]) {
 		//Create WrappedUp object and test the methods
-		
+		WrappedUp wu = new WrappedUp();
+		wu.guessMyFavouriteThing();
 	}
 }

0423105ada5918ed4f292ea807bab60266ebb434
hkon123 <hkonhd@gmail.com>
2020-11-09 13:02:03 +0100

    Changed Folder structure

---
+++ Week2-lab1/Car/TestCar.java
@@ -0,0 +1,14 @@
+
+public class TestCar {
+
+	public static void main(String[] args) {
+		//Create Car object and text car methods 
+		Car car = new Car();
+		car.setMake("Toyota");
+		car.setPrice(10000);
+		car.setRegistration("AB 432756");
+		car.displayDetails();
+
+	}
+
+}
---
+++ Week2-lab1/Klingon/TestKlingonShip.java
@@ -0,0 +1,13 @@
+
+
+public class TestKlingonShip {
+
+	public static void main(String[] args) {
+		
+		KlingonShip ks = new KlingonShip();
+		ks.displayKlingonDetails();
+		ks.promptSettings();
+		ks.displayKlingonDetails();
+	}
+
+}

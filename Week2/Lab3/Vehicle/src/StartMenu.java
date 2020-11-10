669e1651a92e03742d10be84b30d4bdc418a3100
hkon123 <hkonhd@gmail.com>
2020-11-10 16:28:27 +0100

    W2 L4 E3 (Added GUI menu to vehicle exercise from CH8)

---
+++ Week2/Lab3/Vehicle/src/StartMenu.java
@@ -0,0 +1,10 @@
+
+public class StartMenu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		vehicleTester st = new vehicleTester();
+		st.runMenu();
+	}
+
+}
--- Week2/Lab3/Vehicle/src/vehicleTester.java
+++ Week2/Lab3/Vehicle/src/vehicleTester.java
@@ -1,11 +1,21 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 
 
 public class vehicleTester {
+	
+	private String alternatives[] = {"Add", "Display", "Remove", "Exit"};
+	private String newOrUsed[] = {"New", "Used"};
+	private ArrayList<Vehicle> vehicles;
+	
+	public vehicleTester() {
+		vehicles = new ArrayList<>();
+	}
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int option =-1;
 		ArrayList<Vehicle> vehicles = new ArrayList<>();
@@ -42,8 +52,8 @@ public class vehicleTester {
 			
 		} while (option!=4);
 	}
-	
-	public static void printMenu() {
+	*/
+	public void printMenu() {
 		System.out.println("Option 1: Add a vehicle");
 		System.out.println("Option 2: Display a list of vehicle details");
 		System.out.println("Option 3: Delete a vehicle");
@@ -51,31 +61,74 @@ public class vehicleTester {
 		System.out.println("Select from [1-4]!");
 	}
 	
-	public static Vehicle addVehicle() {
+	public Vehicle addVehicle() {
 		Vehicle tempVehicle; 
-		System.out.print("Is the vehicle new(n) or secondhand(s)?");
-		Scanner sc = new Scanner(System.in);
-		Scanner ssc = new Scanner(System.in);
-		char choice = sc.next().charAt(0);
-		System.out.print("Registration number: ");
-		String regNo = ssc.next();
-		System.out.print("Make: ");
-		String make = ssc.next();
-		System.out.print("Manufacture Year: ");
-		int year = sc.nextInt();
-		System.out.print("value: ");
-		double value = sc.nextDouble();
-		if (choice == 'n' || choice == 'N') {
+		//System.out.print("Is the vehicle new(n) or secondhand(s)?");
+		//Scanner sc = new Scanner(System.in);
+		//Scanner ssc = new Scanner(System.in);
+		int choice = JOptionPane.showOptionDialog(null, "Is the car new or used?","Vehicle menu",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, newOrUsed, newOrUsed[0]);
+		//char choice = sc.next().charAt(0);
+		//System.out.print("Registration number: ");
+		//String regNo = ssc.next();
+		String regNo = JOptionPane.showInputDialog(null,"Enter the Registration number:","Reg number", JOptionPane.PLAIN_MESSAGE);
+		//System.out.print("Make: ");
+		String make = JOptionPane.showInputDialog(null,"Enter the make:","Make", JOptionPane.PLAIN_MESSAGE);
+		//System.out.print("Manufacture Year: ");
+		int year = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter the Year the car was made:","Year", JOptionPane.PLAIN_MESSAGE));
+		//System.out.print("value: ");
+		double value = Double.parseDouble(JOptionPane.showInputDialog(null,"Enter the value of the car:","Value", JOptionPane.PLAIN_MESSAGE));
+		if (choice == 0) {
 			tempVehicle = new Vehicle(regNo, make, year, value);
 			return tempVehicle;
 		}
 		else {
-			System.out.print("Total owners: ");
-			int owners = sc.nextInt();
+			//System.out.print("Total owners: ");
+			int owners = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter the amount of owners this car has had:","Owners", JOptionPane.PLAIN_MESSAGE));
 			tempVehicle = new SecondHandVehicle(regNo, make, year, value, owners);
 			return tempVehicle;
 		}
 	}
+	
+	 public void runMenu()
+     {
+        
+        int option = getChoice();
+        while (option != 3)
+        {
+        	switch(option) {
+			case(0):
+				vehicles.add(addVehicle());
+				break;
+			case(1):
+				System.out.println("-----------Printing details!-----------");
+				for (int i=0; i<vehicles.size(); i++) {
+					System.out.println("\n\nVehicle nr:" + i);
+					System.out.println(vehicles.get(i));
+				}
+				break;
+			case(2):
+				//System.out.print("Which vehicle would you like to remove? (tip: print details to get vehicle ID: ");
+				int id = Integer.parseInt(JOptionPane.showInputDialog(null,"Which vehicle would you like to remove?\n (tip: print details to get vehicle ID: ","Remove", JOptionPane.PLAIN_MESSAGE));
+				vehicles.remove(id);
+				break;
+			case(3):
+				System.out.println("Exiting program!");
+				break;
+			default:
+				System.out.println("Enter a number between 1-4");
+				break;
+		}
+         option = getChoice();
+        }//End while
+     }
+    
+    public int getChoice()
+    {
+        int choice = JOptionPane.showOptionDialog(null, "Select from ..","Vehicle Menu",
+                     JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, alternatives, alternatives[0]);
+        return choice;
+    }
 	
 }
 

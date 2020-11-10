fe87cf489163fe564bd3756f42e1c3257d6c9135
hkon123 <hkonhd@gmail.com>
2020-11-10 10:40:38 +0100

    CH 8 Q8.a

---
+++ Week2/Lab3/LectureEquipment/src/Booking.java
@@ -0,0 +1,17 @@
+
+public class Booking {
+	String room, name;
+	
+	public Booking(String roomIn, String nameIn) {
+		room = roomIn;
+		name = nameIn;
+	}
+	
+	public String getRoom() {
+		return room;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}

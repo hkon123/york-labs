31b4e56841d082e71a785abe5a39a68c0fa553d9
hkon123 <hkonhd@gmail.com>
2020-11-10 10:07:23 +0100

    CH8 Q3.a

--- Week2/Lab3/StockItem/src/StockItem.java
+++ Week2/Lab3/StockItem/src/StockItem.java
@@ -1,4 +1,43 @@
 
 public class StockItem {
+	
+	private String stockNumber, name;
+	private double price;
+	private int totalStock;
+	
+	public StockItem(String stockNumberIn, String nameIn, double priceIn) {
+		stockNumber = stockNumberIn;
+		name = nameIn;
+		price = priceIn;
+		totalStock = 0;
+	}
+	
+	public void setPrice(double priceIn) {
+		price = priceIn;
+	}
+	
+	public void increaseTotalStock(int stockIn) {
+		totalStock += stockIn;
+	}
+	
+	public String getStockNumber() {
+		return stockNumber;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getTotalStock() {
+		return totalStock;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public double calculateTotalPrice() {
+		return price * totalStock;
+	}
 
 }
